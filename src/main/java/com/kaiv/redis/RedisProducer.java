f6fd59fa378651d9b35e47936cbce25997c7c9dc
ivan.kavatsiv <ivan.kavatsiv@globallogic.com>
2025-04-04 14:32:13 +0300

    test commit to develop branch

--- src/main/java/com/kaiv/redis/RedisProducer.java
+++ src/main/java/com/kaiv/redis/RedisProducer.java
@@ -29,7 +29,7 @@ public class RedisProducer implements CommandLineRunner {
     @Override
     public void run(String... args) {
 
-        // test
+        // test !
         for (int i = 0; i <= 999999; i++) {
             try {
                 Thread.sleep(2000);
